c61c93dab2ed0ecb2cf8f21293b62c356f4dedae
Adam Macken <mackenadam@gmail.com>
2021-02-18 15:09:27 +1300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+        }
+
+        // Syntax
+        // <Access modifier> (Static) <Return value> <Name> (Paramaters (<Data type> <Name>))
+        // i.e
+        public static void WriteSomething()
+        {
+            Console.WriteLine("Called from method.");
+            Console.Read();
+        }
+    }
+}
